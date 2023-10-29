eb637ec3b2dc81cf7d03d18ae7b1a60347d2d0ac
Duresa Eshetu <121906025+Durdure@users.noreply.github.com>
2023-10-29 13:56:35 +0300

    Time: 67 ms (57.39%), Space: 17.5 MB (39.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Double for-loop by boute force : time and space complecity O(n^2)
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        
+        #by using the dictionary O(n) notation
+        dic = {}
+        for i, num in enumerate(nums):
+            if target - num in dic:
+                return [dic[target - num], i]
+            elif num not in dic:
+                dic[num]=i
